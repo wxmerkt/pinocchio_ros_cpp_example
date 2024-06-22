d000ca64c1b7f44838ee48357f6de4d6da3935b4
Wolfgang Merkt <w.merkt@gmail.com>
2024-06-22 14:25:10 +0100

    Fix linking example

--- pinocchio_ros_example/src/pinocchio_example.cpp
+++ pinocchio_ros_example/src/pinocchio_example.cpp
@@ -2,18 +2,20 @@
 
 #include "pinocchio/algorithm/joint-configuration.hpp"
 #include "pinocchio/algorithm/geometry.hpp"
+#include "pinocchio/parsers/sample-models.hpp"
 
-int main(int argc, char * argv[])
+int main(int /*argc*/, char * /*argv*/[])
 {
   // Create a set of Pinocchio models and data
   pinocchio::Model model;
+  pinocchio::buildModels::manipulator(model);
   pinocchio::Data data(model);
   pinocchio::GeometryModel collision_model;
   pinocchio::GeometryData collision_data(collision_model);
 
   // Uncomment and try to compile collision code
-  // Eigen::VectorXd q(2);
-  // auto result = pinocchio::computeCollisions(model, data, collision_model, collision_data, q);
+  Eigen::VectorXd q = pinocchio::neutral(model);
+  auto result = pinocchio::computeCollisions(model, data, collision_model, collision_data, q);
 
   return 0;
 }
